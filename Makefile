f0d783807efdc1b029696ca39d550da3306b3449
Manuel Guillermo Cruz Sales <mcruz-sa@c2r14s5.42wolfsburg.de>
2024-04-02 13:14:06 +0200

    Add Makefile, libft and .h files

---
+++ Makefile
@@ -0,0 +1,49 @@
+NAME	= minishell
+
+#LIBFT
+LIBFT_DIRECTORY  = ./libft/
+LIBFT	= $(LIBFT_DIRECTORY)libft.a
+
+OBJ_DIR	= obj/
+CC		= gcc
+CFLAGS	= -Wall -Wextra -Werror -g
+
+SRC = 	main.c \
+
+all: $(NAME)
+
+OBJ = $(patsubst %.c, $(OBJ_DIR)%.o, $(SRC))
+
+$(LIBFT):
+		make -C $(LIBFT_DIRECTORY)
+
+$(NAME): $(OBJ) $(LIBFT)
+			$(CC) $(CFLAGS) $(OBJ) $(LIBFT) -o $(NAME)
+			@echo "$(GREEN)$(NAME) created!$(DEFAULT)"
+
+$(OBJ_DIR)%.o: %.c
+			@mkdir -p $(@D)
+			@$(CC) $(CFLAGS) -I$(LIBFT_DIRECTORY) -c $< -o $@
+
+clean:
+		rm -rf $(OBJ_DIR)
+		@make clean -C $(LIBFT_DIRECTORY)
+		@echo "$(YELLOW)Object files deleted!$(DEFAULT)"
+
+fclean: clean
+		@rm -f ./$(NAME)
+		@rm -f ./$(LIBFT)
+		@echo "$(RED)All files deleted!$(DEFAULT)"
+
+re: fclean all
+
+norminette: 
+		norminette $(SRC) $(SUPPORT_DIR) minishell.h $(LIBFT_DIRECTORY)
+
+.PHONY: all clean fclean re
+
+#COLORS
+RED = \033[1;31m
+GREEN = \033[1;32m
+YELLOW = \033[1;33m
+DEFAULT = \033[0m
---
+++ minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcruz-sa <mcruz-sa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: demrodri <demrodri@student.42wolfsburg.de>*/
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 13:10:22 by mcruz-sa          #+#    #+#             */
+/*   Updated: 2024/04/02 13:10:23 by mcruz-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "./libft/libft.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+
+#endif
